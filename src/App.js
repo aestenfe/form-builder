39be177cc88874c891d664317a7615436a0cd02b
Andreas Estenfelder <andreasestenfelder@Andreass-MacBook-Air.local>
2021-01-26 21:52:42 -0500

    Implemented render check and small cleanup

--- src/App.js
+++ src/App.js
@@ -9,9 +9,6 @@ import Title from "./components/Title";
 
 function App() {
   const [formData, setFormData] = useState([]);
-  // TODO: Use values variable for form fields
-  // and remove eslint-disable
-  // eslint-disable-next-line no-unused-vars
   const [values, setValues] = useState([]);
 
   const importJsonData = () => {
@@ -35,14 +32,62 @@ function App() {
     importJsonData();
   }, []);
 
+  const updateValue = (id, value) => {
+    setValues(values.map((element) => {
+      if (element.id === id) {
+        return { ...element, value };
+      }
+      return element;
+    }));
+  };
+
+  const handleValueChange = ({ target: { name, value } }) => {
+    updateValue(parseInt(name, 10), value);
+  };
+
+  const handleSelectChange = ({ value }, { name }) => {
+    updateValue(name, value);
+  };
+
+  const handleClickValidate = () => {
+    console.log(values);
+    // eslint-disable-next-line no-alert
+    alert("Valid form");
+  };
+
+  const renderCheck = (data) => {
+    if (!data.render) {
+      return true;
+    }
+
+    let result = false;
+    data.render.forEach((rules) => {
+      let { length } = rules.rule;
+      rules.rule.forEach((rule) => {
+        const value = values.find((element) => element.id === rule[0]);
+        if (value && value.value === rule[1]) {
+          length -= 1;
+        }
+      });
+      if (length === 0) {
+        result = true;
+      }
+    });
+    return result;
+  };
+
   const createField = (data) => {
     switch (data.type) {
-      case "text":
-        return <Input type="text" />;
+      case "text": {
+        const value = values.find((element) => data.id === element.id);
+        return <Input type="text" name={data.id} value={value ? value.value : ""} onChange={handleValueChange} />;
+      }
       case "select":
         return (
           <Select
-            options={data.options.map((option) => ({ label: option.name, value: option.name }))}
+            options={data.options.map((option) => ({ label: option.name, value: option.id }))}
+            name={data.id}
+            onChange={handleSelectChange}
             theme={(theme) => ({
               ...theme,
               colors: {
@@ -65,12 +110,16 @@ function App() {
     <FormContainer>
       <Title>Form Builder</Title>
       {formData ? formData.map((data) => (
-        <FormElement key={data.id}>
-          <p>{data.question}</p>
-          {createField(data)}
-        </FormElement>
-      )) : null}
-      <Button>Post Job</Button>
+        <div key={data.id}>
+          {renderCheck(data) ? (
+            <FormElement question={data.question} isRequired={data.required}>
+              {createField(data)}
+            </FormElement>
+          ) : null}
+        </div>
+      ))
+        : null}
+      <Button onClick={handleClickValidate}>Post Job</Button>
     </FormContainer>
   );
 }
